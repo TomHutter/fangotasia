2d0d0dc7e24aed78a1e016b0d7eb72d0b90317cb
Tom Hutter <tom.hutter@unixum.de>
2020-11-07 15:50:21 +0000

    package config

--- fantasia.go
+++ fantasia.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fantasia/config"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -14,55 +15,6 @@ import (
 
 	"gopkg.in/yaml.v2"
 )
-
-const (
-	// https://en.wikipedia.org/wiki/ANSI_escape_code
-	red     = "\033[01;31m"
-	green   = "\033[01;32m"
-	yellow  = "\033[01;33m"
-	blue    = "\033[01;34m"
-	white   = "\033[01;97m"
-	neutral = "\033[0m"
-)
-
-var areas = [52][4]int{
-	{}, {0, 0, 2, 0}, {8, 0, 3, 1}, {9, 0, 0, 2}, {10, 0, 5, 0},
-	{11, 0, 0, 4}, {13, 0, 7, 0}, {0, 8, 0, 0}, {7, 2, 9, 0},
-	{15, 3, 10, 8}, {0, 4, 11, 9}, {0, 5, 12, 10}, {0, 0, 0, 11},
-	{16, 6, 0, 0}, {17, 0, 15, 0}, {0, 9, 0, 14}, {0, 13, 17, 0},
-	{0, 14, 18, 16}, {24, 0, 19, 17}, {25, 0, 20, 18}, {0, 0, 21, 19},
-	{26, 0, 0, 20}, {27, 0, 23, 0}, {0, 0, 24, 22}, {29, 18, 0, 23},
-	{30, 19, 0, 0}, {32, 21, 0, 0}, {0, 22, 28, 0}, {0, 0, 0, 27},
-	{0, 0, 30, 0}, {0, 25, 0, 0}, {0, 0, 0, 0}, {0, 26, 0, 33},
-	{0, 34, 32, 0}, {33, 36, 37, 35}, {0, 0, 34, 0}, {34, 0, 5, 0},
-	{37, 38, 39, 34}, {37, 38, 38, 38}, {40, 39, 39, 37}, {42, 39, 0, 0},
-	{43, 41, 42, 41}, {44, 40, 42, 41}, {47, 41, 43, 43}, {48, 42, 45, 44},
-	{49, 45, 45, 44}, {50, 51, 0, 0}, {47, 43, 48, 47}, {48, 44, 48, 47},
-	{0, 45, 50, 0}, {0, 46, 0, 49}, {46, 0, 0, 0},
-}
-
-var areaMap = [12][10]int{
-	{0, 0, 0, 0, 0, 0, 47, 48, 49, 50},
-	{0, 0, 0, 0, 0, 0, 43, 44, 45, 46},
-	{0, 0, 0, 0, 0, 0, 41, 42, 0, 51},
-	{0, 0, 0, 0, 33, 32, 0, 40, 0, 0},
-	{0, 0, 0, 35, 34, 0, 37, 39, 0, 0},
-	{27, 28, 29, 30, 36, 0, 38, 0, 0, 0},
-	{22, 23, 24, 25, 0, 26, 0, 0, 0, 0},
-	{16, 17, 18, 19, 20, 21, 0, 0, 0},
-	{13, 14, 15, 0, 0, 0, 0, 0, 0, 0},
-	{6, 7, 0, 31, 0, 0, 0, 0, 0, 0},
-	{0, 8, 9, 10, 11, 12, 0, 0, 0, 0},
-	{1, 2, 3, 4, 5, 0, 0, 0, 0, 0},
-}
-
-var objectsInArea = [45][2]int{
-	{-1, 0}, {}, {}, {}, {}, {}, {}, {}, {},
-	{28, 0}, {29, 0}, {8, 0}, {24, 0}, {2, 0}, {26, 0}, {20, 10}, {19, 0}, {19, 22},
-	{18, 0}, {18, 20}, {16, 0}, {13, 0}, {14, 0}, {0, 0}, {0, 26}, {15, 5}, {8, 5},
-	{9, 0}, {11, 0}, {12, 0}, {1, 0}, {1, 0}, {1, 0}, {3, 7}, {4, 0}, {4, 0},
-	{4, 0}, {0, 0}, {0, 0}, {0, 18}, {30, 0}, {33, 10}, {51, 0}, {40, 0}, {51, 47},
-}
 
 var doorOpen = false
 
@@ -90,7 +42,7 @@ func appendText(block *[]string, newText string, color ...string) {
 	if color == nil {
 		*block = append(text, newText)
 	}
-	*block = append(text, fmt.Sprintf("%s%s%s", color[0], newText, white))
+	*block = append(text, fmt.Sprintf("%s%s%s", color[0], newText, config.WHITE))
 }
 
 func printScreen(text []string) {
@@ -115,12 +67,14 @@ func getBoxLen(locations []string) int {
 	return boxLen + 4
 }
 
-func drawBox(area int, boxLen int, locations []string) (box [3]string) {
+func drawBox(area int, boxLen int, locations []string) (box [5]string) {
 	if area == 0 {
-		horLine := strings.Repeat(" ", boxLen)
-		box[0] = fmt.Sprintf("%s", horLine)
-		box[1] = fmt.Sprintf("%s", horLine)
-		box[2] = fmt.Sprintf("%s", horLine)
+		spacer := strings.Repeat(" ", boxLen)
+		box[0] = fmt.Sprintf("%s", spacer)
+		box[1] = fmt.Sprintf("%s", spacer)
+		box[2] = fmt.Sprintf("%s", spacer)
+		box[3] = fmt.Sprintf("%s", spacer)
+		box[4] = fmt.Sprintf("%s", spacer)
 		return
 	}
 	var leftCon, rightCon, topCon, bottomCon string
@@ -128,31 +82,62 @@ func drawBox(area int, boxLen int, locations []string) (box [3]string) {
 	textLen := len([]rune(text)) + 2 // one space left and right
 	leftBuffer := strings.Repeat(" ", (boxLen-textLen)/2)
 	rightBuffer := strings.Repeat(" ", boxLen-len(leftBuffer)-textLen)
-	horLine := strings.Repeat("\u2501", (boxLen-3)/2)
-	if areas[area][3] == 0 {
-		leftCon = "\u2503"
+	empty := strings.Repeat(" ", textLen-2)
+	horLine := strings.Repeat(config.HL, (boxLen-3)/2)
+	if config.Areas[area][3] == 0 {
+		leftCon = config.VL
 	} else {
-		leftCon = "\u252B"
+		leftCon = config.LC
 	}
-	if areas[area][2] == 0 {
-		rightCon = "\u2503"
+	if config.Areas[area][2] == 0 {
+		rightCon = config.VL
 	} else {
-		rightCon = "\u2523"
+		rightCon = config.RC
 	}
-	if areas[area][0] == 0 {
-		topCon = "\u2501"
+	if config.Areas[area][0] == 0 {
+		topCon = config.HL
 	} else {
-		topCon = "\u253B"
+		topCon = config.TC
 	}
-	if areas[area][1] == 0 {
-		bottomCon = "\u2501"
+	if config.Areas[area][1] == 0 {
+		bottomCon = config.HL
 	} else {
-		bottomCon = "\u2533"
+		bottomCon = config.BC
 	}
-	box[0] = fmt.Sprintf("\u250F%s%s%s\u2513", horLine, topCon, horLine)
-	box[1] = fmt.Sprintf("%s%s%s%s%s", leftCon, leftBuffer, text, rightBuffer, rightCon)
-	box[2] = fmt.Sprintf("\u2517%s%s%s\u251B", horLine, bottomCon, horLine)
+	box[0] = fmt.Sprintf("%s%s%s%s%s", config.BTL, horLine, topCon, horLine, config.BTR)
+	box[1] = fmt.Sprintf("%s%s%s%s%s", config.VL, leftBuffer, empty, rightBuffer, config.VL)
+	box[2] = fmt.Sprintf("%s%s%s%s%s", leftCon, leftBuffer, text, rightBuffer, rightCon)
+	box[3] = fmt.Sprintf("%s%s%s%s%s", config.VL, leftBuffer, empty, rightBuffer, config.VL)
+	box[4] = fmt.Sprintf("%s%s%s%s%s", config.BBL, horLine, bottomCon, horLine, config.BBR)
 	return
+}
+
+//func topCon(box int) {
+//	if areas[box][0] == 0 {
+//
+//}
+
+func drawMap(x, y int, locations []string) {
+	if x > 7 {
+		x = 7
+	}
+	if y > 8 {
+		y = 8
+	}
+	boxLen := getBoxLen(locations)
+	spacer := strings.Repeat(" ", boxLen/2)
+	for i := y; i < y+4; i++ {
+		box1 := drawBox(config.AreaMap[i][x], boxLen, locations)
+		box2 := drawBox(config.AreaMap[i][x+1], boxLen, locations)
+		box3 := drawBox(config.AreaMap[i][x+2], boxLen, locations)
+		fmt.Printf("%s\u2503%s    %s\u2503%s    %s\u2503%s\n", spacer, spacer, spacer, spacer, spacer, spacer)
+		fmt.Printf("%s    %s    %s\n", box1[0], box2[0], box3[0])
+		fmt.Printf("%s    %s    %s\n", box1[1], box2[1], box3[1])
+		fmt.Printf("%s\u2501\u2501\u2501\u25b6%s\u2501\u2513  %s\n", box1[2], box2[2], box3[2])
+		fmt.Printf("%s    %s\u25c4\u251B  %s\n", box1[3], box2[3], box3[3])
+		fmt.Printf("%s    %s    %s\n", box1[4], box2[4], box3[4])
+		fmt.Printf("%s\u25B2%s    %s\u2503%s    %s\u2503%s\n", spacer, spacer, spacer, spacer, spacer, spacer)
+	}
 }
 
 // Setup keyboard scanning
@@ -168,9 +153,9 @@ func scanner() (r rune) {
 func prelude() {
 	var text []string
 
-	appendText(&text, "fantasia", red)
-	appendText(&text, "- Ein Adventure von Klaus Hartmuth -", blue)
-	appendText(&text, "- Überarbeitet von Tom Hutter -", yellow)
+	appendText(&text, "fantasia", config.RED)
+	appendText(&text, "- Ein Adventure von Klaus Hartmuth -", config.BLUE)
+	appendText(&text, "- Überarbeitet von Tom Hutter -", config.YELLOW)
 	printScreen(text)
 	//scanner()
 }
@@ -197,7 +182,7 @@ func flash(text []string, err string) {
 	flashText := make([]string, len(text))
 	copy(flashText, text)
 	flashText = append(text, "")
-	flashText = append(text, fmt.Sprintf("%s%s", red, err))
+	flashText = append(text, fmt.Sprintf("%s%s", config.RED, err))
 	printScreen(flashText)
 	time.Sleep(2 * time.Second)
 	printScreen(text)
@@ -205,11 +190,11 @@ func flash(text []string, err string) {
 
 func surroundings(area int, locations []string, objects []string) (text []string) {
 	if area == 25 {
-		objectsInArea[40][0] = 25
+		config.ObjectsInArea[40][0] = 25
 		objects[40-9] = "eine Tür im Norden"
 	}
 	if area == 30 {
-		objectsInArea[40][0] = 30
+		config.ObjectsInArea[40][0] = 30
 		objects[40-9] = "eine Tür im Süden"
 	}
 
@@ -217,31 +202,31 @@ func surroundings(area int, locations []string, objects []string) (text []string
 	//	ifoa=30thenge(40)=30:ge$(40)="eine tuer im sueden"
 	//fmt.Printf("Ich bin %s\n", locations[area-1])
 	//var text []string
-	text = append(text, fmt.Sprintf("%sArea: %d [N:%d,S:%d,O:%d,W:%d]", white, area, areas[area][0], areas[area][1], areas[area][2], areas[area][3]))
-	text = append(text, fmt.Sprintf("%sIch bin %s", yellow, locations[area-1]))
+	text = append(text, fmt.Sprintf("%sArea: %d [N:%d,S:%d,O:%d,W:%d]", config.WHITE, area, config.Areas[area][0], config.Areas[area][1], config.Areas[area][2], config.Areas[area][3]))
+	text = append(text, fmt.Sprintf("%sIch bin %s", config.YELLOW, locations[area-1]))
 	//appendText(&text, fmt.Sprintf("Ich bin %s", locations[area-1]), yellow)
 	var items []string
-	for i, v := range objectsInArea {
+	for i, v := range config.ObjectsInArea {
 		if v[0] == area {
 			//items = append(items, objects[i-9])
 			item := objects[i-9]
 			if strings.Contains(item, "::") {
 				item = strings.ReplaceAll(item, "::", "")
-				items = append(items, fmt.Sprintf("%s  - %s", white, item))
+				items = append(items, fmt.Sprintf("%s  - %s", config.WHITE, item))
 				//appendText(&text, fmt.Sprintf("  - %s", item), white)
 			} else {
-				items = append(items, fmt.Sprintf("%s  - %s", blue, item))
+				items = append(items, fmt.Sprintf("%s  - %s", config.BLUE, item))
 				//appendText(&text, fmt.Sprintf("  - %s", item), blue)
 			}
 		}
 	}
 	if len(items) > 0 {
 		text = append(text, "")
-		text = append(text, fmt.Sprintf("%sIch sehe:", blue))
+		text = append(text, fmt.Sprintf("%sIch sehe:", config.BLUE))
 		for _, item := range items {
 			text = append(text, item)
 		}
-		text = append(text, neutral)
+		text = append(text, config.NEUTRAL)
 	}
 	printScreen(text)
 	return
@@ -290,7 +275,7 @@ func move(area int, direction int, text []string) int {
 	//if direction == 0 {
 	//	return 0, "Ich brauche eine Richtung."
 	//}
-	if areas[area][direction] == 0 {
+	if config.Areas[area][direction] == 0 {
 		flash(text, "In diese Richtung führt kein Weg.")
 		return area
 	}
@@ -299,7 +284,7 @@ func move(area int, direction int, text []string) int {
 		flash(text, "Die Tür ist versperrt.")
 		return area
 	}
-	return areas[area][direction]
+	return config.Areas[area][direction]
 }
 
 func useDoor() {
@@ -337,16 +322,9 @@ func main() {
 	c.getConf("locations.yaml")
 	locations := c.Locations
 
-	boxLen := getBoxLen(locations)
-	for i := 0; i < 12; i++ {
-		box1 := drawBox(areaMap[i][1], boxLen, locations)
-		box2 := drawBox(areaMap[i][2], boxLen, locations)
-		box3 := drawBox(areaMap[i][3], boxLen, locations)
-		fmt.Printf("%s%s%s\n", box1[0], box2[0], box3[0])
-		fmt.Printf("%s%s%s\n", box1[1], box2[1], box3[1])
-		fmt.Printf("%s%s%s\n", box1[2], box2[2], box3[2])
-	}
+	drawMap(0, 7, locations)
 	return
+
 	/*
 		box1 := drawBox(1, boxLen, locations)
 		box2 := drawBox(2, boxLen, locations)
